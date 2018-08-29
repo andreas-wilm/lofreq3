d7e134279a7b0136afd135b720bf21876cfe55b7
Andreas WILM <wilma@gis.a-star.edu.sg>
2018-08-29 10:09:26 +0800

    introduced type PlpObj for json parsing and made changes to meet nim naming convention

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # LoFreq Version 3
+
+Run `nimble test` to run tests (see ./tests)
+
+Run `nimble build` to build the binary (see `./lofreq`)
+
--- src/lofreqpkg/call.nim
+++ src/lofreqpkg/call.nim
@@ -23,38 +23,43 @@ import math
 import strutils
 import times
 
+type PlpObj = object
+  sq: string
+  pos: Natural
+  refStr: string
+  qHist: Table[string, Table[int, int]]
 
 ##
-## brief Computes log(exp(log_a) + exp(log_b))
+## brief Computes log(exp(logA) + exp(logB))
 ##
 ## Taken from util.h of FAST source code:
 ## http://www.cs.cornell.edu/~keich/FAST/fast.tar.gz
-proc log_sum(log_a: float32, log_b: float32): float32 =
-  if log_a > log_b:
-    log_a + ln(1.0 + exp(log_b-log_a))# FIXME use c log1p?
+proc logSum(logA: float32, logB: float32): float32 =
+  if logA > logB:
+    logA + ln(1.0 + exp(logB-logA))# FIXME use c log1p?
   else:
-    log_b + ln(1.0 + exp(log_a-log_b))# FIXME use c log1p?
+    logB + ln(1.0 + exp(logA-logB))# FIXME use c log1p?
 
     
 # FIXME add doc and refer to paper
 # FIXME: does it make sense to receive phred scores here instead?
 # less conversion and no need to check range then?
 # FIXME no need to make public except for tests
-proc pruned_prob_dist*(err_probs: openArray[float],# FIXME use ref to safe mem?
-                      K: Natural,
-                      bonf = 1.0, sig = 1.0): seq[float] =
+# FIXME add pseudocode from paper here
+proc prunedProbDist*(errProbs: openArray[float],# FIXME use ref to safe mem?
+                     K: Natural,
+                     bonf = 1.0, sig = 1.0): seq[float] =
   assert K > 0
-  let N = err_probs.len
-  var probvec = newSeq[float64](K+1)
-  var probvec_prev = newSeq[float64](K+1)
+  var probVec = newSeq[float64](K+1)
+  var probVecPrev = newSeq[float64](K+1)
     
-  for f in err_probs:
+  for f in errProbs:
     assert f>=0.0 and f<=1.0
-  probvec_prev[0] = 0.0; # log(1.0)
+  probVecPrev[0] = 0.0; # log(1.0)
 
-  for n in 1..N:
+  for n in 1..errProbs.len:
     var k = 0;
-    let pn = err_probs[n-1]
+    let pn = errProbs[n-1]
     var log_pn = 0.0'f64
     var log_1_pn = 0.0'f64
 
@@ -71,36 +76,36 @@ proc pruned_prob_dist*(err_probs: openArray[float],# FIXME use ref to safe mem?
        log_1_pn = ln(1.0-pn)# /* 0.0 = log(1.0) # FIXME use c log1p?
       
     if n < K:
-      probvec_prev[n] = -float(high(int))
+      probVecPrev[n] = -float(high(int))
       
     for k in countdown(min(n, K-1), 1):
       try:# FIXME why try?
-        assert probvec_prev[k]<=0.0 and probvec_prev[k-1]<=0.0
+        assert probVecPrev[k]<=0.0 and probVecPrev[k-1]<=0.0
       except AssertionError:
         raise
-      probvec[k] = log_sum(probvec_prev[k] + log_1_pn,
-                           probvec_prev[k-1] + log_pn)
+      probvec[k] = logSum(probVecPrev[k] + log_1_pn,
+                           probVecPrev[k-1] + log_pn)
       
     k = 0;
-    assert probvec_prev[k] <= 0.0
-    probvec[k] = probvec_prev[k] + log_1_pn
+    assert probVecPrev[k] <= 0.0
+    probvec[k] = probVecPrev[k] + log_1_pn
     
     if n==K:
-      probvec[K] = probvec_prev[K-1] + log_pn;
+      probvec[K] = probVecPrev[K-1] + log_pn;
       # FIXME prune here as well?
        
     elif n > K:
       try:# FIXME why try?
-        assert probvec_prev[K] <= 0.0 and probvec_prev[K-1] <= 0.0 # FIXME
+        assert probVecPrev[K] <= 0.0 and probVecPrev[K-1] <= 0.0 # FIXME
       except AssertionError:
         raise
-      probvec[K] = log_sum(probvec_prev[K], probvec_prev[K-1] + log_pn)
+      probvec[K] = logSum(probVecPrev[K], probVecPrev[K-1] + log_pn)
 
       let pvalue = exp(probvec[K]);
           
       # FIXME store as phred scores instead?
       # Q = -10*log_10(e^X), where X=probvec[K]
-      # remember, log_b(x) = log_k(x)/log_k(b), i.e. log_10(Y) = log_e(Y)/log_e(10)
+      # remember, logB(x) = log_k(x)/log_k(b), i.e. log_10(Y) = log_e(Y)/log_e(10)
       # therefore, Q = -10 * log_e(e^X)/log_e(10) = -10 * X/log_e(10)
       # e.g.
       # >>> from math import log, log10, e
@@ -114,23 +119,29 @@ proc pruned_prob_dist*(err_probs: openArray[float],# FIXME use ref to safe mem?
       if pvalue * bonf > sig:
         # explicitly limiting to valid range
         return probvec[0..K]
-    swap(probvec, probvec_prev)
+    swap(probvec, probVecPrev)
 
   # return prev because we just swapped (if not pruned)
-  return probvec_prev[0..K] # explicitly limiting to valid range
+  return probVecPrev[0..K] # explicitly limiting to valid range
 
     
-proc parse_plp_json(fname: string): Table[string, Table[int, int]]  =
-  result = initTable[string, Table[int, int]]()
+proc parsePlpJson(fname: string): PlpObj =
+  result.qHist = initTable[string, Table[int, int]]()
   
   # let dataJson = parseJson(data)
   let dataJson = parseFile(fname)
   
   # assert used in nim in action after parsing json string
   assert dataJson.kind == JObject
-
-  for event, qhist in dataJson.pairs():
-    for qual, count in qhist.pairs():
+  # FIXME should we validate the keys before starting to parse?
+  # what about extra keys for example. should they be ignored?
+  
+  result.sq = dataJson["sq"].getStr
+  result.pos =  dataJson["pos"].getInt
+  result.refStr = dataJson["ref"].getStr
+  
+  for event, qHist in dataJson["qhist"].pairs():
+    for qual, count in qHist.pairs():
       var q = 0      
       assert qual[0] == 'Q'
       # FIXME: string slicing and toInt simply doesn't work. Try: var
@@ -142,10 +153,10 @@ proc parse_plp_json(fname: string): Table[string, Table[int, int]]  =
 
       # FIXME allow for lowercase i.e bases mapping to rev strand.
       # Best done elsewhere and not here
-      if not result.hasKey(event):
-        result[event] = initTable[int, int]()
-      assert result[event].hasKey(q) == false
-      result[event][q] = c
+      if not result.qHist.hasKey(event):
+        result.qHist[event] = initTable[int, int]()
+      assert result.qHist[event].hasKey(q) == false
+      result.qHist[event][q] = c
 
       
 proc prob2qual*(e: float): Natural =
@@ -158,13 +169,13 @@ proc qual2prob*(q: Natural): float =
   return pow(10.0, float(-q)/10.0)
 
     
-proc datestr(): string = 
+proc dateStr(): string = 
   var t = getTime().local()
   result = t.format("yyyyMMdd")
 
 
 # FIXME check conformity
-proc write_vcf_header(src="FIXME:src", reffa="FIXME:ref") =
+proc writeVcfHeader(src="FIXME:src", refFa="FIXME:refWhichWeOnlyKnowDuringPlp") =
   var hdr = """##fileformat=VCFv4.2
 ##fileDate=$1
 ##source=$2
@@ -174,36 +185,16 @@ proc write_vcf_header(src="FIXME:src", reffa="FIXME:ref") =
 ##INFO=<ID=SB,Number=1,Type=Integer,Description="Phred-scaled strand bias at this position">
 ##INFO=<ID=DP4,Number=4,Type=Integer,Description="Counts for ref-forward bases, ref-reverse, alt-forward and alt-reverse bases">
 ##INFO=<ID=INDEL,Number=0,Type=Flag,Description="Indicates that the variant is an INDEL.">
-#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO""" % [datestr(), src, reffa]
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO""" % [dateStr(), src, refFa]
   echo hdr
 
     
-proc call*(plp_fname: string) =
-  #var plpTable: Table[char, Table[int]]
-  #for base in "ACGTN":
-  #  plpTable[base] = initTable[int]()
-    
-  # populate with dummy data
-  #for base in "ACGTN":
-  #  for qual in [20, 30]:
-  #    plpTable[base][qual] = int(qual/10)
-  #echo(plpTable)
-  # {'A': {20: 2, 30: 3}, 'C': {20: 2, 30: 3}, 'G': {20: 2, 30: 3}, 'N': {20: 2, 30: 3}, 'T': {20: 2, 30: 3}}
- 
+proc call*(plpFname: string) =
+  writeVcfHeader()
+  var plp = parsePlpJson(plpFname)
+  echo("FIXME: compute pvalue for ", plp)
 
-  #var data = %*
-  #  [
-  #     {"A": {"20": 2, "30": 3}, "C": {"20": 2, "30": 3}, "G": {"20": 2, "30": 3}, "N": {"20": 2, "30": 3}, "T": {"20": 2, "30": 3}, "+AC": {"20": 2, "30": 3}, "-TG": {"20": 2, "30": 3}}
-  #  ]
-  #parse_plp_json($data)
-  # when passed as string we get a JArray?!
-  # when passed as file we get JObject?!
-  write_vcf_header()
- 
-  # plpTable =
-  var plpTable = parse_plp_json(plp_fname)
-  echo(plpTable)
-      
+  
 when isMainModule:
   import cligen
   dispatch(call)
