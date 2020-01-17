9e9b7e34d52aa8852aeb74f1df542cea212aaabd
Andreas Wilm <andreas.wilm@microsoft.com>
2020-01-17 22:16:07 +0800

    Simplified region regexp in region.nim (and dos2unix)

--- src/lofreqpkg/region.nim
+++ src/lofreqpkg/region.nim
@@ -21,22 +21,22 @@ proc `$`*(r: Region): string =
 proc reg_from_str*(regStr: string): Region =
   ## expects region string in the form of seq[:start-end],
   ## where start-end are inclusive and 1-based (and >0).
-  ## indeces are stored as zero based, half open interval.
+  ## indices are stored as zero based, half open interval.
   ## if start and end  are not given, both will be set to 0
-  var matches = regStr.match(re"(\w+):(\d+)-(\d+)")
+  var matches = regStr.match(re"(.+):(\d+)-(\d+)")
   if matches.isSome:
-    # chr:start-end 
+    # chr:start-end
     result.sq = matches.get.captures[0]
     # FIXME test correct boundaries
     result.s = matches.get.captures[1].parseInt-1
     result.e = matches.get.captures[2].parseInt
     doAssert result.s >= 0 and result.s < result.e# FIXME rather raise exception
   else:
-    # chrom only 
+    # chrom only
     doAssert regStr.contains(':') == false# FIXME proper error message
     result.sq = regStr
     # s=e means no indices given
     result.s = 0
-    result.e = 0  
+    result.e = 0
   doAssert len(result.sq)>0
   
