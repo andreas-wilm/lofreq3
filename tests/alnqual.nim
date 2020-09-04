f632a268ed1b8b1886b405dc33038eff0a8d4586
Andreas Wilm <andreas.wilm@microsoft.com>
2020-09-04 11:27:19 +0800

    alnqual: completed

---
+++ tests/alnqual.nim
@@ -0,0 +1,54 @@
+# standard library
+import unittest
+import math
+import osproc
+#import strutils
+import tempfile
+import strformat
+
+# project specific
+import ../src/lofreqpkg/alnqual
+#import ../src/lofreqpkg/utils
+
+# third party
+import hts
+
+
+let lofreq = "../lofreq"
+
+
+suite "alignment qualities (baq, ai, ad)":
+
+  test "simple":
+    const fa = "alnqual/ref.fa"
+    const bam = "alnqual/alignments.bam"
+
+    # make sure call works and  nothing is predicted
+    var tmpfd: File
+    var tmpname: string
+    (tmpfd, tmpname) = mkstemp()
+    tmpfd.close
+    let cmd = fmt"{lofreq} alnqual -f {fa} -i {bam} > {tmpname}"
+    echo "Testing: " & cmd
+    let outp = execProcess(cmd)
+
+    var b:Bam
+    open(b, tmpname, index=false, fai=fa)
+    for record in b:# only one record in there
+      let ai = tag[string](record, AI_TAG)
+      check not ai.isNone
+      check ai.get == "~~~!~~~"
+
+      let ad = tag[string](record, AD_TAG)
+      check not ad.isNone
+      check ad.get == "!~~~~~~"
+      
+      let baq = tag[string](record, BAQ_TAG)
+      check not baq.isNone
+      check baq.get == "!!!!55!"
+      
+      #  ../lofreq.git/src/lofreq/lofreq alnqual $bam $fa
+      #@HD     VN:1.4  SO:coordinate
+      #@SQ     SN:ref  LN:14
+    # 1       0       ref     1       60      1M2D3M2I1M      *       0       0       AAAAGCC 5555555 BI:Z:???????    BD:Z:IIIIIII lb:Z:!!!!55!     ai:Z:~~~!~~~    ad:Z:!~~~~~~
+
---
+++ tests/alnquals.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -u
+#set -ux
+
+# should really write a nim program to test this
+# rather than running lofreq three times
+# in bash
+
+# a BAM with reads containing indels
+inbam=NC_000912_Mpneumoniae/viterbi/NC_000912_Mpneumoniae_comb.srt.indelonly.bam
+test -e $inbam
+# fasta
+fasta=NC_000912_Mpneumoniae/NC_000912_Mpneumoniae.fasta
+test -e $fasta
+# the reference BAM produced with 2.1.5 alnqual
+refbam=NC_000912_Mpneumoniae/alnqual/NC_000912_Mpneumoniae_comb.srt.indelonly.alnqual.bam
+test -e $refbam
+# the test bam
+testbam=${refbam%alnqual.bam}.newAlnqual.bam
+
+# run new alnqual
+../lofreq alnqual -f $fasta -i $inbam | samtools view -b -o $testbam
+
+ndiff=$(diff <(../lofreq alnqual -f $fasta -i $inbam | grep -Pow 'ai:Z:.*[^\t]' | cut -f 1) <(samtools view $refbam | grep -Pow 'ai:Z:.*[^\t]' | cut -f 1) | grep -c '^>')
+if [ $ndiff -gt 0 ]; then
+    echo "FAIL: old and new alnqual AI implementations differ"
+    exit 1
+fi
+
+ndiff=$(diff <(../lofreq alnqual -f $fasta -i $inbam | grep -Pow 'ad:Z:.*[^\t]' | cut -f 1) <(samtools view $refbam | grep -Pow 'ad:Z:.*[^\t]' | cut -f 1) | grep -c '^>')
+if [ $ndiff -gt 0 ]; then
+    echo "FAIL: old and new alnqual AD implementations differ"
+    exit 1
+fi
+
+ndiff=$(diff <(../lofreq alnqual -f $fasta -i $inbam | grep -Pow 'lb:Z:.*[^\t]' | cut -f 1) <(samtools view $refbam | grep -Pow 'lb:Z:.*[^\t]' | cut -f 1) | grep -c '^>')
+if [ $ndiff -gt 0 ]; then
+    echo "FAIL: old and new alnqual BAQ implementations differ"
+    exit 1
+fi
+
+echo "OK: alnqual implementations give identical results"
+
+rm $testbam
